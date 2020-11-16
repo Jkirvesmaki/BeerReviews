24755bae2383bc8974a36829d8ea56fbd7e00c19
Jkirvesmaki <joonas.kirvesmaki@gmail.com>
2020-11-16 22:28:35 +0200

    1

--- src/main/java/com/example/Arvostelu/model/Category.java
+++ src/main/java/com/example/Arvostelu/model/Category.java
@@ -15,7 +15,7 @@ import javax.persistence.OneToMany;
 @Entity
 public class Category {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	
 	private Long categoryId;
 	private String name;
