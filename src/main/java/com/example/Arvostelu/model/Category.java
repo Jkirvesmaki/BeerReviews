47d04b6bee0115a4edc8ee855ec70332d9f10062
Jkirvesmaki <joonas.kirvesmaki@gmail.com>
2020-11-16 23:23:01 +0200

    Update Category.java

--- src/main/java/com/example/Arvostelu/model/Category.java
+++ src/main/java/com/example/Arvostelu/model/Category.java
@@ -16,7 +16,7 @@ import javax.persistence.Table;
 @Entity
 public class Category {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	
 	private Long categoryId;
 	private String name;
